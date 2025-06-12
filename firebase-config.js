255c197919df04b74e707dd79dcc7b3b6c64e473
Mugishapro <nidoctorai@gmail.com>
2025-06-12 02:26:05 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDg3UPQARAGjWdwF53vJIwYOCA7hOIfG1s",
+  authDomain: "muganga-ai.firebaseapp.com",
+  projectId: "muganga-ai",
+  storageBucket: "muganga-ai.firebasestorage.app",
+  messagingSenderId: "609789687173",
+  appId: "1:609789687173:web:04b6b1eeb20cc70062c0ec",
+  measurementId: "G-JSTCX5WQ31"
+};
